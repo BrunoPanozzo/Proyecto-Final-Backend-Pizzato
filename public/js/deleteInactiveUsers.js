5ce5919e3229263c8bb7dc794e721b241d799052
VeroPizzato <verizzato@gmail.com>
2024-08-06 09:03:13 -0300

    subo cambios

--- public/js/deleteInactiveUsers.js
+++ public/js/deleteInactiveUsers.js
@@ -1,10 +1,7 @@
-const deleteUsersButtons = document.querySelectorAll('.eliminar-usuarios-inactivos')
-
-deleteUsersButtons.forEach(button => {
-    button.addEventListener('click', e => {
-        e.preventDefault()
-        
-        // Realizo una solicitud de eliminación al servidor
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("eliminar-usuarios-inactivos")
+    btn.addEventListener("click", function () {
+        // Realizo una solicitud de eliminación de usuarios inactivos al servidor
         fetch('/api/users/', {
             method: 'DELETE',            
         }).then(result => {            
@@ -16,3 +13,22 @@ deleteUsersButtons.forEach(button => {
         })
     })
 })
+
+// const deleteUsersButtons = document.querySelectorAll('.eliminar-usuarios-inactivos')
+
+// deleteUsersButtons.forEach(button => {
+//     button.addEventListener('click', e => {
+//         e.preventDefault()
+        
+//         // Realizo una solicitud de eliminación al servidor
+//         fetch('/api/users/', {
+//             method: 'DELETE',            
+//         }).then(result => {            
+//             if (result.status === 200) {
+//                 window.location.reload()  // se recarga la página actual
+//             }
+//         }).catch(error => {
+//             console.error('Error al realizar la solicitud:', error)
+//         })
+//     })
+// })
--- public/js/deleteUser.js
+++ public/js/deleteUser.js
@@ -1,23 +1,10 @@
-const deleteUserButtons = document.querySelectorAll('.eliminar-usuario')
-
-deleteUserButtons.forEach(button => {
-    button.addEventListener('click', e => {
-        e.preventDefault()
-
-        // Accedo al formulario padre del botón
-        const form = button.closest('form')
-        if (!form) {
-            console.error('No se encontró un formulario asociado al botón.')
-            return
-        }
-
-        // Obtengo el ID del usuario que deseo eliminar
-        const userId = form.getAttribute('user-id')
-
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("eliminar-usuario")
+    btn.addEventListener("click", function () {
         // Realizo una solicitud de eliminación al servidor
-        fetch(`/api/users/${userId}`, {
+        fetch(`/api/users/${btn.id}`, {
             method: 'DELETE',
-        }).then(result => {            
+        }).then(result => {
             if (result.status === 200) {
                 window.location.reload()  // se recarga la página actual
             }
@@ -26,3 +13,32 @@ deleteUserButtons.forEach(button => {
         })
     })
 })
+
+// const deleteUserButtons = document.querySelectorAll('.eliminar-usuario')
+
+// deleteUserButtons.forEach(button => {
+//     button.addEventListener('click', e => {
+//         e.preventDefault()
+
+//         // Accedo al formulario padre del botón
+//         const form = button.closest('form')
+//         if (!form) {
+//             console.error('No se encontró un formulario asociado al botón.')
+//             return
+//         }
+
+//         // Obtengo el ID del usuario que deseo eliminar
+//         const userId = form.getAttribute('user-id')
+
+//         // Realizo una solicitud de eliminación al servidor
+//         fetch(`/api/users/${userId}`, {
+//             method: 'DELETE',
+//         }).then(result => {
+//             if (result.status === 200) {
+//                 window.location.reload()  // se recarga la página actual
+//             }
+//         }).catch(error => {
+//             console.error('Error al realizar la solicitud:', error)
+//         })
+//     })
+// })
