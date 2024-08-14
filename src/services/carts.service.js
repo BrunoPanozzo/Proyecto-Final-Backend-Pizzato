d21bf9fa4d41eaef8d604e1c9487ffbe46b56e81
VeroPizzato <verizzato@gmail.com>
2024-08-14 19:18:22 -0300

    subo cambios

--- src/services/carts.service.js
+++ src/services/carts.service.js
@@ -24,10 +24,10 @@ class CartsService {
     }
 
     async addProductToCart (cartId, prodId, quantity) { 
-        const product = await this.productsService.getProductById(prodId)         
+        const product = await this.productsService.getProductById(prodId)                 
         if (!product)
             return false       
-        const userOwner = await this.jwtServices.getUserByEmail(product.owner)     
+        const userOwner = await this.jwtServices.getUserByEmail(product.owner)  
         if (!userOwner)
             return false       
         const userCart = await this.jwtServices.getUserByCartId(cartId)         
